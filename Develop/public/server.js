3bea87c003710ce1aad83a826548da761c51095c
Lucas W <winslowlucas8@gmail.com>
2020-12-05 23:13:30 -0500

    fixing heroku errors

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -20,7 +20,7 @@ var notes = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 app.get("/notes", function(req, res) {
